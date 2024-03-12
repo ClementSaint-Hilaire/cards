a3a9281c426b813a5b6711dc7d0d990ba20f023e
ClémentSaintHilaire <clement.sainthilaire.pro@gmail.com>
2024-03-12 13:12:28 +0100

    update back

--- php/accueil.php
+++ php/accueil.php
@@ -15,43 +15,26 @@
         include '../configuration/database.php';
     ?>
 
-   
-<div class="recherche_container">
-    <button class="loupe"><svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g clip-path="url(#clip0_97_10300)">
-            <path d="M3 10C3 10.9193 3.18106 11.8295 3.53284 12.6788C3.88463 13.5281 4.40024 14.2997 5.05025 14.9497C5.70026 15.5998 6.47194 16.1154 7.32122 16.4672C8.1705 16.8189 9.08075 17 10 17C10.9193 17 11.8295 16.8189 12.6788 16.4672C13.5281 16.1154 14.2997 15.5998 14.9497 14.9497C15.5998 14.2997 16.1154 13.5281 16.4672 12.6788C16.8189 11.8295 17 10.9193 17 10C17 9.08075 16.8189 8.1705 16.4672 7.32122C16.1154 6.47194 15.5998 5.70026 14.9497 5.05025C14.2997 4.40024 13.5281 3.88463 12.6788 3.53284C11.8295 3.18106 10.9193 3 10 3C9.08075 3 8.1705 3.18106 7.32122 3.53284C6.47194 3.88463 5.70026 4.40024 5.05025 5.05025C4.40024 5.70026 3.88463 6.47194 3.53284 7.32122C3.18106 8.1705 3 9.08075 3 10Z" stroke="#1D1D1F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M21 21L15 15" stroke="#1D1D1F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-        </g>
-        <defs>
-            <clipPath id="clip0_97_10300">
-                <rect width="24" height="24" fill="white"/>
-            </clipPath>
-        </defs>
-    </svg>
-</button>
-    <form action="./accueil.php" method="GET" class="rechercher">
-        <input type="text" name="search" placeholder="Rechercher par catégorie">
-        <button type="submit">Rechercher</button>
-        <button class="close">fermer</button>
-    </form>
-</div>
-
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-    var loupeButton = document.querySelector('.loupe');
-    var closeButton = document.querySelector('.close');
-    var form = document.querySelector('.rechercher');
-
-    loupeButton.addEventListener('click', function() {
-        form.style.display = 'flex';
-    });
-
-    closeButton.addEventListener('click', function() {
-        form.style.display = 'none';
-    });
-});
-</script>
-
+    <div class="recherche_container">
+        <button class="loupe">
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_97_10300)">
+                    <path d="M3 10C3 10.9193 3.18106 11.8295 3.53284 12.6788C3.88463 13.5281 4.40024 14.2997 5.05025 14.9497C5.70026 15.5998 6.47194 16.1154 7.32122 16.4672C8.1705 16.8189 9.08075 17 10 17C10.9193 17 11.8295 16.8189 12.6788 16.4672C13.5281 16.1154 14.2997 15.5998 14.9497 14.9497C15.5998 14.2997 16.1154 13.5281 16.4672 12.6788C16.8189 11.8295 17 10.9193 17 10C17 9.08075 16.8189 8.1705 16.4672 7.32122C16.1154 6.47194 15.5998 5.70026 14.9497 5.05025C14.2997 4.40024 13.5281 3.88463 12.6788 3.53284C11.8295 3.18106 10.9193 3 10 3C9.08075 3 8.1705 3.18106 7.32122 3.53284C6.47194 3.88463 5.70026 4.40024 5.05025 5.05025C4.40024 5.70026 3.88463 6.47194 3.53284 7.32122C3.18106 8.1705 3 9.08075 3 10Z" stroke="#1D1D1F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M21 21L15 15" stroke="#1D1D1F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </g>
+                <defs>
+                    <clipPath id="clip0_97_10300">
+                        <rect width="24" height="24" fill="white"/>
+                    </clipPath>
+                </defs>
+            </svg>
+        </button>
+        <form action="./accueil.php" method="GET" class="rechercher">
+            <input type="text" name="search" placeholder="Rechercher par catégorie">
+            <button type="submit">Rechercher</button>
+            <button class="close">fermer</button>
+        </form>
+    </div>
 
     <?php
 
@@ -91,7 +74,7 @@ document.addEventListener('DOMContentLoaded', function() {
             if (!$results_found) { // Si aucun résultat n'a été trouvé
                 echo '<p>Aucun résultat trouvé pour cette recherche.</p>';
             }
-            
+
         } else {
             echo "Erreur lors de l'exécution de la requête : " . $query->errorInfo()[2];
         }
@@ -100,4 +83,21 @@ document.addEventListener('DOMContentLoaded', function() {
     ?>
 
 </body>
+
+    <script>
+    document.addEventListener('DOMContentLoaded', function() {
+        var loupeButton = document.querySelector('.loupe');
+        var closeButton = document.querySelector('.close');
+        var form = document.querySelector('.rechercher');
+
+        loupeButton.addEventListener('click', function() {
+            form.style.display = 'flex';
+        });
+
+        closeButton.addEventListener('click', function() {
+            form.style.display = 'none';
+        });
+    });
+    </script>
+
 </html>
