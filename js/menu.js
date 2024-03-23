fc8967e5dc7febced72dba96652adc4b9c70a887
cshd <clement.sainthilaire@sts-sio-caen.info>
2024-03-23 21:45:16 +0100

    navbar responsive

--- js/menu.js
+++ js/menu.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var contentR = document.querySelector('.content_right');
     var contentL = document.querySelector('.content_left');
     var liens = document.querySelector('.liens');
+    var pages = document.querySelector('.pages');
 
     menuButton.addEventListener('click', function() {
         navbar.classList.add('navbar_mobile');
@@ -14,12 +15,13 @@ document.addEventListener('DOMContentLoaded', function() {
         contentR.classList.add('content_right_mobile');
         contentL.classList.add('content_left_mobile');
         liens.classList.add('liens_mobile');
+        pages.classList.add('pages_mobile');
 
         menuButton.style.display = 'none';
         closeButton.style.display = 'flex';
         menuPage.forEach(function(page) {
             page.style.display = 'flex';
-        });    
+        });  
     });
 
     closeButton.addEventListener('click', function() {
@@ -28,6 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
         contentR.classList.remove('content_right_mobile');
         contentL.classList.remove('content_left_mobile');
         liens.classList.remove('liens_mobile');
+        pages.classList.remove('pages_mobile');
 
         menuPage.forEach(function(page) {
             page.style.display = 'none';
